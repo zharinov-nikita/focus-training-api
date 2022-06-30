91671795c383f18ee29af2a322f656c48e9d2144
Nikita <zharinov.nicita@gmail.com>
2022-06-30 16:17:43 +0700

    [feature/api-key] #1
    Теперь по запросу на любой роутер, который был отправлен одним из http методов будет обрабатываться через app.middleware, который не пропустить запросы без Api key

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
-import { Module } from '@nestjs/common'
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
+import { AppMiddleware } from './app.middleware'
+
 @Module({
   imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AppMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL })
+  }
+}
