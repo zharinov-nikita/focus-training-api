b0f1c01d980446d2e2c4f3d4aebd3b2a7e9b2d12
Nikita <zharinov.nicita@gmail.com>
2022-06-30 16:02:52 +0700

    [feature/api-key] Config .env
    Настройка переменных окуржения

--- src/app.middleware.ts
+++ src/app.middleware.ts
@@ -3,5 +3,11 @@ import { Request, Response, NextFunction } from 'express'
 
 @Injectable()
 export class AppMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {}
+  use(req: Request, res: Response, next: NextFunction) {
+    const isApiKey = req.headers?.['api-key']
+    if (isApiKey) {
+      const apiKeyClient = req.headers['api-key']
+      const apiKeyServer = 1
+    }
+  }
 }
