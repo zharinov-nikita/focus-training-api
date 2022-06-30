543a30a917e68137b8cede476ec32aa8bc71c017
Nikita <zharinov.nicita@gmail.com>
2022-06-30 16:10:42 +0700

    [feature/api-key] Обработка запросов без api-key, с api-key не правильным и с api-key правильным

--- src/app.middleware.ts
+++ src/app.middleware.ts
@@ -7,7 +7,22 @@ export class AppMiddleware implements NestMiddleware {
     const isApiKey = req.headers?.['api-key']
     if (isApiKey) {
       const apiKeyClient = req.headers['api-key']
-      const apiKeyServer = 1
+      const apiKeyServer = process.env['api-key']
+
+      if (apiKeyClient === apiKeyServer) {
+        return next()
+      }
+
+      return res.status(401).json({
+        statusCode: 401,
+        level: 'error',
+        message: 'The wrong key was specified when logging in to the api!',
+      })
     }
+    return res.status(403).json({
+      statusCode: 403,
+      level: 'error',
+      message: 'Access for unauthorized users is prohibited!',
+    })
   }
 }
