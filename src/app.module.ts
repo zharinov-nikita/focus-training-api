179b0ffcccfd26fd04caec6f769a0be044335bab
Nikita <zharinov.nicita@gmail.com>
2022-06-30 17:27:28 +0700

    [feature/trainer-module] #7

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,11 +9,13 @@ import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { AppMiddleware } from './app.middleware'
 import { MongooseModule } from '@nestjs/mongoose'
+import { TrainerModule } from './trainer/trainer.module'
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot('mongodb://localhost:27017/focus-training-api'),
+    TrainerModule,
   ],
   controllers: [AppController],
   providers: [AppService],
